1fcc0de4ae7108478347d0cb086d07b2530ca9a7
DariaSavu <143889190+DariaSavu@users.noreply.github.com>
2025-05-04 14:39:03 +0300

    Change java version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,12 +4,16 @@ module inventory {
     requires javafx.controls;
     requires java.logging;
 
-    opens inventory.model to javafx.base;
+    opens inventory.model to javafx.base, org.junit.platform.commons;
     exports inventory.model;
+
     opens inventory to javafx.fxml;
     exports inventory;
+
     opens inventory.controller to javafx.fxml;
     exports inventory.controller;
 
     exports inventory.service;
+
+    opens inventory.repository to org.junit.platform.commons;
 }
